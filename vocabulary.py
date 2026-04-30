64ec3db09e9dab7ee1fee2a8bdc8f15dcfe56aa8
Tsun Wai Wong <tswong@student.42berlin.de>
2026-04-30 21:44:08 +0200

    feat: add method to search for certain vocabs

--- vocabulary.py
+++ vocabulary.py
@@ -4,8 +4,9 @@ from llm_sdk import Small_LLM_Model
 
 
 class Vocabulary:
-    def __init__(self, vocab_path):
-        self.vocabs = self._get_all_vocabs(vocab_path)
+    def __init__(self, model):
+        self.vocab_path = model.get_path_to_vocab_file()
+        self.vocabs = self._get_all_vocabs(self.vocab_path)
 
 
     def _get_all_vocabs(self, vocab_path) -> List[str]:
@@ -24,33 +25,42 @@ class Vocabulary:
             raise Exception("Vocab file cannot be opened"
                             "due to permission error")
 
+    # get from regular expression
+    
+    def search_for_vocab(self, targets: List[str]) -> Dict[str, int] | None:
+        valid_tokens = set()
+        for vocab, token_id in self.vocabs.items():
+            if vocab in targets:
+                valid_tokens.add(token_id)
+        return valid_tokens
+
     def get_valid_tokens_match_str(self, patterns: List[str], generated_text: str) -> Dict[str, int]:
         """
         return all valid potential next tokens which can match the string pattern
         """
-        valid_tokens = {}
+        valid_tokens = set()
         for vocab, token_id in self.vocabs.items():
             prefix = generated_text + vocab
             if any(pattern.startswith(prefix) for pattern in patterns):
-                valid_tokens[vocab] = token_id
+                valid_tokens.add(token_id)
         return valid_tokens
 
     def get_valid_tokens_match_token(self, token_sequences: List[List[int]], generated_tokens: List[int]) -> Dict[str, int]:
         """
         return all valid potential next tokens which can match the token id list pattern
         """
-        valid_tokens = {}
-        for vocab, token_id in self.vocabs.items():
+        valid_tokens = set()
+        for _, token_id in self.vocabs.items():
             prefix = generated_tokens + [token_id]
             if any(sequence[:len(prefix)] == prefix for sequence in token_sequences):
-                valid_tokens[vocab] = token_id
+                valid_tokens.add(token_id)
         return valid_tokens
 
 
 if __name__ == "__main__":
     model = Small_LLM_Model()
-    path = model.get_path_to_vocab_file()
-    vocab = Vocabulary(path)
+    print(model.get_path_to_vocab_file())
+    vocab = Vocabulary(model)
 
     pattern1 = "Hello, how are you today?"
     pattern2 = "Hello, how old are you now?"
@@ -60,3 +70,8 @@ if __name__ == "__main__":
     print([encoded1, encoded2])
     result = vocab.get_valid_tokens_match_token([encoded1, encoded2], generated_tokens)
     print(result)
+    print(vocab.search_for_vocab([".", "]", "}"]))
+
+    print(model.decode([16485, 5746, 25, 5168, 2891, 32964, 11, 5168, 1889]))
+    #print(model.decode([2610, 1410, 990, 279, 2701, 5746, 25, 5168, 2891]))
+    # print(model.decode([5097, 510, 1302, 3744, 3712, 41861, 382, 13314, 510]))
